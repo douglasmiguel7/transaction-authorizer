8c2b12d2ffa038d94bd0a2149ff7aced8543bff5
Douglas Andrade <douglasandrademiguel@gmail.com>
2024-07-31 20:45:36 -0300

    chore: fix account table migration

--- src/main/resources/db/migration/V20240731003557__add_account_table.sql
+++ src/main/resources/db/migration/V20240731003557__add_account_table.sql
@@ -1,8 +1,8 @@
 create table account (
     id uuid not null default gen_random_uuid(),
-    cash numeric(10,2),
-    food numeric(10,2),
-    meal numeric(10,2),
+    cash numeric(10,2) not null default 0,
+    food numeric(10,2) not null default 0,
+    meal numeric(10,2) not null default 0,
 
     constraint account_pk primary key (id)
 )
