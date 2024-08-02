720661865a356841401561e987f35bc7674334be
Douglas Andrade <douglasandrademiguel@gmail.com>
2024-08-02 18:00:04 -0300

    chore: remove unused code

--- src/main/kotlin/com/gh/douglasmiguel7/transactionAuthorizer/core/enumx/Mcc.kt
+++ src/main/kotlin/com/gh/douglasmiguel7/transactionAuthorizer/core/enumx/Mcc.kt
@@ -1,19 +1,9 @@
 package com.gh.douglasmiguel7.transactionAuthorizer.core.enumx
 
 enum class Mcc(
-  val codes: List<String>,
+  val codes: List<String> = listOf(),
 ) {
   FOOD(codes = listOf("5411", "5412")),
   MEAL(codes = listOf("5811", "5812")),
-  CASH(codes = listOf());
-
-  companion object {
-    fun byCode(value: String): Mcc {
-      val mcc = entries.find { entry ->
-        entry.codes.any { code -> code == value }
-      }
-
-      return mcc ?: CASH
-    }
-  }
+  CASH,
 }
